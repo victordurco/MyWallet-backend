da88b31aec7455f7842aa204c97c51d284a4245c
victordurco <victordurco@gmail.com>
2021-10-20 23:34:12 -0300

    sign-up route done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 
+import { registerUser } from "./controllers/sign-up.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("salve");
-});
+app.get("/sign-up", registerUser);
 
 app.listen(4000); // start server
--- src/controllers/sign-up.js
+++ src/controllers/sign-up.js
@@ -1,5 +1,33 @@
 import connection from "../database/database.js";
+import { signUpSchema } from "../validations/signUp.js";
+import bcrypt from "bcrypt";
 
 const registerUser = async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body;
+    const userData = req.body;
+    const { name, email, password } = req.body;
+
+    const { error } = signUpSchema.validate(userData);
+    if (error) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        await connection.query(
+            `
+                INSERT INTO users
+                (name, email, password)
+                VALUES ($1, $2, $3)
+            `,
+            [name, email, passwordHash]
+        );
+
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 };
+
+export { registerUser };
